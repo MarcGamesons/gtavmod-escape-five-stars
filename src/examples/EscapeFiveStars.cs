e7dca6dd860c3b4bd9a42bc5c09d542ad65af284
MarcGamesons <MarcGamesons@users.noreply.github.com>
2017-05-22 18:00:13 +0200

    Press T to Escape
    You can now press T to start a timer. After the timer reaches 0 you
    wanted level si set to 0.

--- src/examples/EscapeFiveStars.cs
+++ src/examples/EscapeFiveStars.cs
@@ -15,11 +15,13 @@ public class EscapeFiveStars : Script
     // This is used to set the time in seconds that you have to survive. DEFAULT is 300 seconds (5 Minutes).
     // If you want to change this value you also have to change the value in line 56 to the same value.
     //
-    int TimeUntilReatreat = 300;
+    int TimeUntilReatreat = 60;
 
     // This timer is used to determine since how many seconds the player is in wanted state.
     //
     int playerWantedSinceSeconds = 0;
+
+    int playerWantedLevel = 0;
 
     // This bool is used to determine if the player has started the escape timer.
     //
@@ -36,78 +38,108 @@ public class EscapeFiveStars : Script
 
     public EscapeFiveStars()
     {
-        // Only Tick if player is in wanted state.
-        //
-        if (Game.Player.WantedLevel == 1 || Game.Player.WantedLevel == 2 || Game.Player.WantedLevel == 3 || Game.Player.WantedLevel == 4 || Game.Player.WantedLevel == 5)
-        {
-            Tick += OnTick;
-            KeyUp += onKeyUp;
-        }
+        Tick += OnTick;
+        KeyUp += onKeyUp;
     }
 
     void OnTick(object sender, EventArgs e)
     {
         if (Game.GameTime > GametimeReference + Cooldown)
         {
+            // Use the cooldown.
+            //
             GametimeReference = Game.GameTime + Cooldown;
 
-            // Increase the "timer".
+            // Variable to store current wanted level.
             //
-            playerWantedSinceSeconds++;
+            playerWantedLevel = Game.Player.WantedLevel;
 
-            // DEBUG
-            //
-            UI.ShowSubtitle("playerWantedSinceSeconds: " + playerWantedSinceSeconds, 3000);
-
-            /*if(playerWantedSinceSeconds >= 1)
+            switch (playerWantedLevel)
             {
-                if(isEscapeTimerStarted)
-                {
-                    UI.ShowSubtitle("TRUE", 3000);
-                }
-            }*/
-
-            // If you are playing with a mod that uses the 6 star system change the 5 to a 6
-            // 
-            if (Game.Player.WantedLevel == 5)
-            {
-                // If the time runs out...
-                //
-                if (TimeUntilReatreat <= 0)
-                {
-                    // ...set the wanted level of the Player to 0.
-                    //
-                    Game.Player.WantedLevel = 0;
-
-                    // If the wanted level of the Player is 0...
-                    //
-                    if (Game.Player.WantedLevel == 0)
-                    {
-                        // ...reset the timer to 300.
-                        //
-                        TimeUntilReatreat = 300;
-                    }
-                }
-                else
-                {
-                    // Decrease the timer.
-                    //
-                    TimeUntilReatreat--;
-
-                    // Show the time left on screen as a subtitle.
-                    //
-                    UI.ShowSubtitle("Survive! Time until troops retreat: " + TimeUntilReatreat, 3000);
-                }
+                case 1:
+                    ScriptLogic();
+                    break;
+                case 2:
+                    ScriptLogic();
+                    break;
+                case 3:
+                    ScriptLogic();
+                    break;
+                case 4:
+                    ScriptLogic();
+                    break;
+                case 5:
+                    ScriptLogic();
+                    break;
+                default:
+                    break;
             }
         }
     }
 
+    // Detect keyUp events.
+    //
     private void onKeyUp(object sender, KeyEventArgs e)
     {
-        if (e.KeyCode == Keys.H)
+        // Detect if the player pressed the H key.
+        //
+        if (e.KeyCode == Keys.T)
         {
+            // Start the escape timer.
+            //
             isEscapeTimerStarted = true;
-            UI.ShowSubtitle("You have pressed the H key.", 3000);
         }
     }
+
+    private void ScriptLogic()
+    {
+        // Increase the "timer".
+        //
+        playerWantedSinceSeconds++;
+
+        // Notify the player that he/she can start the escape timer.
+        //
+        if (playerWantedSinceSeconds >= 60)
+        {
+            UI.Notify("You can now press T to start the escape timer.", true);
+        }
+
+        // If you are playing with a mod that uses the 6 star system change the 5 to a 6
+        // 
+        if (Game.Player.WantedLevel == 5 || isEscapeTimerStarted)
+        {
+            // If the time runs out...
+            //
+            if (TimeUntilReatreat <= 1)
+            {
+                // ...set the wanted level of the Player to 0.
+                //
+                Game.Player.WantedLevel = 0;
+
+                // If the wanted level of the Player is 0...
+                //
+                if (Game.Player.WantedLevel == 0)
+                {
+                    // Disable the bool that is used to determine if the escape timer is started.
+                    //
+                    isEscapeTimerStarted = false;
+
+                    // ...reset the timer to 300.
+                    //
+                    TimeUntilReatreat = 60;
+                }
+            }
+            else
+            {
+                // Decrease the timer.
+                //
+                TimeUntilReatreat--;
+
+                // Show the time left on screen as a subtitle.
+                //
+                UI.ShowSubtitle("Survive! Time until troops retreat: " + TimeUntilReatreat, 600);
+            }
+        }
+    }
+
 }
